c9a17f269881bd505ea6d3ee546d83d2e0c68062
csr-ph <lucaw10@hotmail.com>
2021-03-29 02:58:17 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,9 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
-console.log(enemyName, enemyHealth, enemyAttack);
 
 var playerMoney = 10;
 
@@ -27,14 +23,14 @@ var fight = function() {
 
         // log a resulting message to the console so we know that it worked.
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
 
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         }
         else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
 
         // subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
@@ -42,7 +38,7 @@ var fight = function() {
 
         // log a resulting message to the console so we know that it worked.
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         // check player's health
@@ -71,4 +67,6 @@ var fight = function() {
     }
 };
 
-fight ();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
